f87304b86c534e6f4ca8bdc8e4ff8c25783f3bcf
Frederick Masterton <frederickmasterton@Fredericks-MacBook-Pro.local>
2024-09-30 00:16:45 -0300

    forgot files

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+// File: next.config.mjs
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
